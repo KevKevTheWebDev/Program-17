3ca033fb0059f8b4eb5c760bf05fbb605e4d5f20
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-21 17:07:55 -0400

    Update and rename README.md to NestedIfStatements.java

---
+++ NestedIfStatements.java
@@ -0,0 +1,12 @@
+class apples{
+	public static void main(String args[]){
+	int age = 60;
+	if (age < 50){
+	System.out.println("You are younger than 50");
+	}else{System.out.println("You are older than 50");
+	if (age > 75){System.out.println("You are much older than 50");
+	}else{System.out.println("But not that much older than 50");
+	}
+}
+}
+}
